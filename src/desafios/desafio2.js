c6d5d29645ce702e3becf9cf705fdaea576fdc06
Fabricio Joaquim <45011367+Fabricio-Joaquim@users.noreply.github.com>
2022-08-07 04:16:51 -0300

    Update desafio2.js

--- src/desafios/desafio2.js
+++ src/desafios/desafio2.js
@@ -1,18 +1,24 @@
 // Como podemos melhorar o esse código usando TS? 
 
-let pessoa1 = {};
+interface IPessoa{
+    nome: string,
+    idade: number,
+    profissao: string,       
+}
+
+let pessoa1:IPessoa = {};
 pessoa1.nome = "maria";
 pessoa1.idade = 29;
 pessoa1.profissao = "atriz"
 
-let pessoa2 = {}
+let pessoa2:IPessoa = {}
 pessoa2.nome = "roberto";
 pessoa2.idade = 19;
 pessoa2.profissao = "Padeiro";
 
 let pessoa3 = {
     nome: "laura",
-    idade: "32",
+    idade: 32,
     profissao: "Atriz"
 };
 
